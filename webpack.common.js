de2f129a4a12c06f7f85c0b5b61d0dc67b9e40d3
matejadb <92729211+matejadb@users.noreply.github.com>
2025-01-15 14:23:29 +0100

    webpack merge

--- webpack.common.js
+++ webpack.common.js
@@ -7,11 +7,27 @@ module.exports = {
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			title: 'Production',
+			template: './src/template.html',
 		}),
 	],
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+		],
+	},
 	output: {
-		filename: '[name].bundle.js',
+		filename: 'main.js',
 		path: path.resolve(__dirname, 'dist'),
 		clean: true,
 	},
